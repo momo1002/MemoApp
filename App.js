c419d8f9c1de4ba530b04a0b5b02cca3d64d5cc0
momo <momoko.nakada1002@gmail.com>
2018-09-05 17:43:25 -0700

    Hide firebase certificate

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 // import { StyleSheet, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import firebase from 'firebase';
 
 // import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
@@ -8,6 +9,8 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+
+import ENV from './env.json';
 
 // export default class App extends React.Component {
 //   render() {
@@ -30,15 +33,27 @@ import SignupScreen from './src/screens/SignupScreen';
 //   },
 // });
 
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.SENDER_ID,
+};
+firebase.initializeApp(config);
+
 const App = createStackNavigator({
+  Login:      { screen: LoginScreen },
+  Signup:     { screen: SignupScreen },
   Home:       { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
-  Login:      { screen: LoginScreen },
-  Signup:     { screen: SignupScreen },
 }, {
   navigationOptions: {
     headerTitle: 'Memot',
+    headerTintColor: '#fff',
+    headerBackTitle: null,
     headerStyle: {
       backgroundColor: '#265366',
     },
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,13 +4,36 @@ import {
 } from 'react-native';
 
 class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>ログイン</Text>
-        <TextInput style={styles.input} value="Email" />
-        <TextInput style={styles.input} value="Password" />
-        <TouchableHighlight style={styles.button} onPress={() => {}}>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text }); }}
+          autoCapitalize="none"
+          placeholder="Email"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }); }}
+          autoCapitalize="none"
+          placeholder="Password"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => { this.handleSubmit.bind(this); }}
+        >
           <Text style={styles.buttonTitle}>ログインする</Text>
         </TouchableHighlight>
       </View>
@@ -52,4 +75,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
+
 export default LoginScreen;
